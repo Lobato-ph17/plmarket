f9897e378b58ce4b8bfcca0d110daee8636afeff
Pedro Lobato <lobato.ph17@gmail.com>
2026-07-22 11:10:36 -0300

    feat: adiciona o endpoint POST para registrar novos produtos

--- src/main/java/com/plmarket/backend/controller/ProdutoController.java
+++ src/main/java/com/plmarket/backend/controller/ProdutoController.java
@@ -2,10 +2,9 @@ package com.plmarket.backend.controller;
 
 import com.plmarket.backend.model.Produto;
 import com.plmarket.backend.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,13 +15,18 @@ public class ProdutoController {
 
     private final ProdutoRepository produtoRepository;
 
-    
     public ProdutoController(ProdutoRepository produtoRepository) {
         this.produtoRepository = produtoRepository;
     }
 
     @GetMapping
     public List<Produto> listarProdutos() {
-        return produtoRepository.findAll(); 
+        return produtoRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> criarProduto(@RequestBody Produto produto) {
+        Produto novoProduto = produtoRepository.save(produto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoProduto);
     }
 }
